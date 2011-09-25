c26dcbe6b1d6be8b1e67d3243e0d0e90e3c6a357
Benson Peng <penglingzhe@gmail.com>
2011-09-25 23:22:16 +0800

    update project

--- FootballScore/Model/SelectMatchTypeController.h
+++ FootballScore/Model/SelectMatchTypeController.h
@@ -6,10 +6,10 @@
 //  Copyright 2011年 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 
-@interface SelectMatchTypeController : NSObject {
+@interface SelectMatchTypeController : UIViewController {
     
 }
 
