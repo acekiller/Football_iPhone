6e7490c8d75b7b6f443050e291f07a3f4ef647d4
Benson Peng <penglingzhe@gmail.com>
2011-09-28 23:20:32 +0800

    add match controller

---
+++ FootballScore.xcodeproj/MatchDetailController.h
@@ -0,0 +1,16 @@
+//
+//  MatchDetailController.h
+//  FootballScore
+//
+//  Created by qqn_pipi on 11-9-28.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MatchDetailController : UIViewController {
+    
+}
+
+@end
