a73ddf02bfad11121157f08589a43cb8452ad85c
kira <ouyongyong@163.com>
2011-09-29 09:13:42 +0800

    add button topLeague,selectAll,selectNone

--- FootballScore/Controller/League/SelectLeagueController.h
+++ FootballScore/Controller/League/SelectLeagueController.h
@@ -15,6 +15,13 @@
     UILabel *promptLabel;
     UIButton *topLeagueButton;
     UIScrollView *scrollView;
+    UIButton *selectNoneButton;
+    UIButton *selectAllButton;
+    
+    NSMutableArray *buttonTagsArray;
+    NSArray *buttonNamesArray;
+    NSArray *topLeagueArray;
+    int buttonNumbers;
 }
 
 -(IBAction)selectAll:(id)sender;
@@ -24,4 +31,6 @@
 
 @property (nonatomic, retain) IBOutlet UIButton *topLeagueButton;
 @property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, retain) IBOutlet UIButton *selectNoneButton;
+@property (nonatomic, retain) IBOutlet UIButton *selectAllButton;
 @end
--- FootballScore/Controller/League/SelectLeagueController.m
+++ FootballScore/Controller/League/SelectLeagueController.m
@@ -13,21 +13,23 @@
 @synthesize promptLabel;
 @synthesize topLeagueButton;
 @synthesize scrollView;
+@synthesize selectNoneButton;
+@synthesize selectAllButton;
 
 
-const float buttonLen = 73;
+const float buttonLen = 70;
 const float buttonHigh = 50;
-const float buttonSepratorX = 5;
+const float buttonSepratorX = 8;
 const float buttonSepratorY = 10;
 const int buttonsPerLine = 4;
 
-NSMutableArray *buttonTagsArray;
-NSArray *buttonNamesArray;
+
 
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {    
     buttonNamesArray = [NSArray arrayWithObjects:@"英超", @"意甲", @"西甲", @"法甲", @"德甲", @"荷甲", @"欧冠", @"国王杯", @"联赛杯", nil];
+    buttonNumbers = [buttonNamesArray count];
     buttonTagsArray = [[NSMutableArray alloc]initWithCapacity:[buttonNamesArray count]];
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
@@ -42,6 +44,8 @@ NSArray *buttonNamesArray;
     [scrollView release];
     [topLeagueButton release];
     [buttonTagsArray release];
+    [selectAllButton release];
+    [selectNoneButton release];
     [super dealloc];
 }
 
@@ -57,11 +61,33 @@ NSArray *buttonNamesArray;
 
 - (void)viewDidLoad
 {
+    
+
+    
+    UIView * rightButtonParentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 44)];
+    rightButtonParentView.backgroundColor = [UIColor clearColor];
+     
+    
+    int buttonSize = 32;
+    int rightOffset = 20;
+    UIButton * setButton = [[UIButton alloc] initWithFrame:CGRectMake(rightButtonParentView.frame.size.width - buttonSize - rightOffset, 6, buttonSize, buttonSize)];
+    [rightButtonParentView addSubview:setButton];
+    [setButton release];
+    
+    UIButton * searchButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 6, buttonSize, buttonSize)];
+    [rightButtonParentView addSubview:searchButton];
+    [searchButton release];
+    
+    
+    UIBarButtonItem * rightButtonItem2 = [[UIBarButtonItem alloc] initWithCustomView:rightButtonParentView];
+    [rightButtonParentView release];                            
+    self.navigationItem.rightBarButtonItem = rightButtonItem2;
+    [rightButtonItem2 release];
   
     int i;
     int rowIndex;
     int rankIndex;
-    for (i=0 ; i<[buttonNamesArray count]; i++){
+    for (i=0 ; i<buttonNumbers; i++){
         NSString *title = [buttonNamesArray objectAtIndex:i];
         UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [button setTitle:title forState:UIControlStateNormal];
@@ -69,7 +95,7 @@ NSArray *buttonNamesArray;
         rowIndex = i/buttonsPerLine;
         rankIndex = i%buttonsPerLine;
         button.frame = CGRectMake(buttonSepratorX+rankIndex*(buttonSepratorX+buttonLen), rowIndex*(buttonHigh+buttonSepratorY), buttonLen, buttonHigh);
-        [button setTag:i];
+        [button setTag:i+1];
         [button setBackgroundImage:[UIImage imageNamed:@"unSelected.png"] forState:UIControlStateNormal];
         [button setSelected:NO];
         [button addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
@@ -86,6 +112,8 @@ NSArray *buttonNamesArray;
     [self setPromptLabel:nil];
     [self setScrollView:nil];
     [self setTopLeagueButton:nil];
+    [self setSelectAllButton:nil];
+    [self setSelectNoneButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -98,14 +126,52 @@ NSArray *buttonNamesArray;
 }
 
 -(IBAction)selectAll:(id)sender{
-    
+    UIButton *selectedButton;
+    NSNumber *findNum;
+    int i = 0;
+    for(i=1;i<=buttonNumbers;i++){
+        selectedButton = [scrollView viewWithTag:i];
+        [selectedButton setBackgroundImage:[UIImage imageNamed:@"selected.png"] forState:UIControlStateNormal];
+        
+        findNum = [NSNumber numberWithInt:i];
+        if(NSNotFound == [buttonTagsArray indexOfObject:findNum]){
+            [buttonTagsArray addObject:findNum];
+        }
+    }   
+        
 }
 
 -(IBAction)selectNone:(id)sender{
+    UIButton *selectedButton;
+    NSNumber *findNum;
+    int i = 0;
+    for(i=1;i<=buttonNumbers;i++){
+        selectedButton = [scrollView viewWithTag:i];
+        [selectedButton setBackgroundImage:[UIImage imageNamed:@"unSelected.png"] forState:UIControlStateNormal];
+
+        }  
+    buttonTagsArray = nil;
     
 }
 
 -(IBAction)selectTopLeague:(id)sender{
+    UIButton *selectedButton;
+    NSNumber *findNum;
+    NSString *buttonTitle;
+    NSArray *topLeagueArray = [NSArray arrayWithObjects:@"英超", @"意甲", @"法甲", nil];
+    int i = 0;
+    for(i=1;i<=buttonNumbers;i++){
+        selectedButton = [scrollView viewWithTag:i];
+        buttonTitle = [selectedButton titleForState:UIControlStateNormal];
+        
+        if(NSNotFound != [topLeagueArray indexOfObject:buttonTitle]){
+            [selectedButton setBackgroundImage:[UIImage imageNamed:@"selected.png"] forState:UIControlStateNormal];
+            findNum = [NSNumber numberWithInt:i];
+            if(NSNotFound == [buttonTagsArray indexOfObject:findNum]){
+                [buttonTagsArray addObject:findNum];
+            }
+        }
+    }   
     
 }
 
