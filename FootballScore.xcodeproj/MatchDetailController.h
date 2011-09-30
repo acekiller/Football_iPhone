d173438f03c2fb1a41852c937fd72b6b6b49bc11
GamyHuang <hguangm2009@gmail.com>
2011-09-30 18:29:01 +0800

    show match detail

--- FootballScore.xcodeproj/MatchDetailController.h
+++ FootballScore.xcodeproj/MatchDetailController.h
@@ -7,10 +7,48 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PPViewController.h"
+#import "MatchService.h"
+#import "Match.h"
 
-
-@interface MatchDetailController : UIViewController {
-    
+@interface MatchDetailController : PPViewController<MatchServiceDelegate> {
+    Match *match;
+    UIImageView *homeTeamIcon;
+    UIImageView *awayTeamIcon;
+    UILabel *matchStateLabel;
+    UILabel *matchStarttimeLabel;
+    UILabel *homeTeamName;
+    UILabel *awayTeamName;
+    UILabel *homeTeamRank;
+    UILabel *awayTeamRank;
+    UIButton *eventButton;
+    UIButton *lineUpButton;
+    UIButton *analysisButton;
+    UIButton *asianOdds;
+    UIButton *auropeanOdds;
+    UIButton *sizeButton;
+    UIWebView *dataWebView;
 }
+
+@property(nonatomic,retain) Match* match;
+-(id)initWithMatch:(Match *)aMatch;
+@property (nonatomic, retain) IBOutlet UIImageView *homeTeamIcon;
+@property (nonatomic, retain) IBOutlet UIImageView *awayTeamIcon;
+@property (nonatomic, retain) IBOutlet UILabel *matchStateLabel;
+@property (nonatomic, retain) IBOutlet UILabel *matchStarttimeLabel;
+@property (nonatomic, retain) IBOutlet UILabel *homeTeamName;
+@property (nonatomic, retain) IBOutlet UILabel *awayTeamName;
+@property (nonatomic, retain) IBOutlet UILabel *homeTeamRank;
+@property (nonatomic, retain) IBOutlet UILabel *awayTeamRank;
+
+@property (nonatomic, retain) IBOutlet UIButton *eventButton;
+@property (nonatomic, retain) IBOutlet UIButton *lineUpButton;
+@property (nonatomic, retain) IBOutlet UIButton *analysisButton;
+@property (nonatomic, retain) IBOutlet UIButton *asianOdds;
+@property (nonatomic, retain) IBOutlet UIButton *auropeanOdds;
+@property (nonatomic, retain) IBOutlet UIButton *sizeButton;
+@property (nonatomic, retain) IBOutlet UIWebView *dataWebView;
+
+
 
 @end
