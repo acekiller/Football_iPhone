f71b078585bc920afdf7dad59045cee8638570ef
Benson Peng <penglingzhe@gmail.com>
2011-10-13 09:46:29 +0800

    add SelectIndexController

---
+++ FootballScore/Controller/Index/SelectIndexController.h
@@ -0,0 +1,16 @@
+//
+//  SelectIndexController.h
+//  FootballScore
+//
+//  Created by qqn_pipi on 11-10-13.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SelectIndexController : UIViewController {
+    
+}
+
+@end
