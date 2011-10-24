9ebf7a92e17fd8cefd982fcb601e42d6b56a8524
Tom Callon <tomcallon2010@gmail.com>
2011-10-24 17:30:23 +0800

    ScoreUpdateCell ：
    1.set the  cell's picts and backgrounds
    2.set the match update datas' font and colors .

--- FootballScore/Controller/Entry/ScoreUpdateCell.m
+++ FootballScore/Controller/Entry/ScoreUpdateCell.m
@@ -12,6 +12,9 @@
 #import "TimeUtils.h"
 #import "LocaleConstants.h"
 #import "MatchManager.h"
+#import "LeagueManager.h"
+#import "Match.h"
+
 @implementation ScoreUpdateCell
 @synthesize leagueName;
 @synthesize startTime;
@@ -67,18 +70,33 @@
    // settingButton.titleLabel.text = message;  用这种方法不行阿 ？？？？调用方法更加好阿 。
     
     NSLog(@"This is a :%@",message);
-    [settingButton setBackgroundImage:image  forState:UIControlStateNormal ] ;
-    [settingButton setTitle:message forState:UIControlStateNormal];
-    [settingButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    
+    [settingButton setBackgroundImage:image  forState:UIControlStateDisabled ] ;
+    [settingButton setTitle:message forState:UIControlStateDisabled];
+    [settingButton setTitleColor:[UIColor whiteColor] forState:UIControlStateDisabled];
 }
 
 
-- (void)setCellInfo:(ScoreUpdate *)scoreUpdate
-{
+- (void)setCellInfo:(ScoreUpdate *)scoreUpdate 
+{ 
+
+    
+    //更新时间 ，颜色
     self.matchState.text =  scoreUpdate.updateMinute;
+    self.matchState.textColor= [UIColor redColor];
+    
+    //比分颜色
+    self.matchScore.textColor= [UIColor greenColor];
+    
     self.startTime.text = dateToChineseStringByFormat([scoreUpdate startTime], @"HH:mm");
+    self.startTime.textColor=[UIColor grayColor];
+    
+    
+    
     self.leagueName.text = [scoreUpdate leagueName];
+    self.leagueName.textColor = [[LeagueManager defaultManager] getLeagueColorById:[[scoreUpdate match]leagueId]];
+    NSLog(@"leagueId = %@",scoreUpdate.match.leagueId);
+   
+    
     self.homeTeam.text = [scoreUpdate homeTeamName];
     self.awayTeam.text =  [scoreUpdate awayTeamName];
 
@@ -95,8 +113,11 @@
         eventImage = [UIImage imageNamed:@"ls_ball.png"];
         self.scoreTypeName.text = FNS(@"比分");
         
+        
         [self setTeamEventButton:type message:FNS(@"进球") image:[UIImage imageNamed:@"ls_img1.png"]];
-                
+            
+    
+        
         self.matchScore.text = [NSString stringWithFormat:@"%@ : %@",
                                 [scoreUpdate homeTeamScore],[scoreUpdate awayTeamScore]];
         
