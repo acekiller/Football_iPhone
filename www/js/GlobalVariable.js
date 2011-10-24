7fd2c43cd8a0d42172cbe759c712d69462a40ecc
Benson Peng <penglingzhe@gmail.com>
2011-10-24 09:46:50 +0800

    add new files

---
+++ www/js/GlobalVariable.js
@@ -0,0 +1,16 @@
+
+// init all global objects
+
+var language = 0;
+
+var oupeiManager = new OupeiManager();
+
+var yapeiManager = new YapeiManager(YAPEI_URL);
+var yapeiCompanyManager = new BetCompanyManager();
+
+// init the global object
+var matchDetailManager = new MatchDetailManager();
+
+
+var matchService = new MatchService();
+
