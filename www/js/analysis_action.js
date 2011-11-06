dced3e1a6404918e56479e06e937230b3d82bfb8
kaibin <ekaibwu@gmail.com>
2011-11-06 13:07:16 +0800

    improve css.

--- www/js/analysis_action.js
+++ www/js/analysis_action.js
@@ -1,28 +1,53 @@
 
 
-function pointsAction() {
+function pointsAction(obj) {
 	var subtable = document.getElementById('table_points');
     subtable.style.display = subtable.style.display == 'none' ? 'block' : 'none';
+	if (subtable.style.display == 'none') {
+		obj.style.background ='url(images/fx_title.png)';
+	} else {
+		obj.style.background ='url(images/fx_title_2.png)';
+	}
 }
 
-function headtoheadActoin() {
+function headtoheadActoin(obj) {
 	var subtable = document.getElementById('table_headtohead');
     subtable.style.display = subtable.style.display == 'none' ? 'block' : 'none';
+	if (subtable.style.display == 'none') {
+		obj.style.background ='url(images/fx_title.png)';
+	} else {
+		obj.style.background ='url(images/fx_title_2.png)';
+	}
 }
 
-function recordsAction() {
+function recordsAction(obj) {
 	var subtable = document.getElementById('table_records');
     subtable.style.display = subtable.style.display == 'none' ? 'block' : 'none';
+	if (subtable.style.display == 'none') {
+		obj.style.background ='url(images/fx_title.png)';
+	} else {
+		obj.style.background ='url(images/fx_title_2.png)';
+	}
 }
 
-function near3gamesAction() {
+function near3gamesAction(obj) {
 	var subtable = document.getElementById('table_near3games');
     subtable.style.display = subtable.style.display == 'none' ? 'block' : 'none';
+	if (subtable.style.display == 'none') {
+		obj.style.background ='url(images/fx_title.png)';
+	} else {
+		obj.style.background ='url(images/fx_title_2.png)';
+	}
 }
 
-function recommendAction() {
+function recommendAction(obj) {
 	var subtable = document.getElementById('table_recommend');
     subtable.style.display = subtable.style.display == 'none' ? 'block' : 'none';
+	if (subtable.style.display == 'none') {
+		obj.style.background ='url(images/fx_title.png)';
+	} else {
+		obj.style.background ='url(images/fx_title_2.png)';
+	}
 }
 
 
