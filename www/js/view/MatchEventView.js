72d115dd8d4a374144b45bd3f4e6957f4ed0ae7f
kaibin <ekaibwu@gmail.com>
2011-11-07 13:56:50 +0800

    improve css.

--- www/js/view/MatchEventView.js
+++ www/js/view/MatchEventView.js
@@ -100,8 +100,21 @@ function MatchDetailView(){
 	    },
 		
 		displayStat : function(type, value) {
-			if (type == "0" && value == "*") { //先开球
+			if (type == "0" && value == "*") {  	//先开球					
 				return "<img src=images/dwq.png />";
+			}
+			if (value == 1) {
+				if (type == "3" || type == "4" || type == "5" || type == "6" 
+				 || type == "7" || type == "8" || type == "9" || type == "10"
+				 || type == "11"|| type == "12"|| type == "13"|| type == "14"
+				 || type == "15"|| type == "17"|| type == "18"|| type == "19"
+				 || type == "20"|| type == "21"|| type == "22"|| type == "23"
+				 || type == "24"|| type == "27"|| type == "29"|| type == "32"
+				 || type == "33" || type == "34") {
+				 	return "<img src=images/dwq.png />";
+				 } else {
+				 	return value;
+				 }
 			}
 			if (type == "2" && value == "1") { //第一张黄牌
 				return "<img src=images/yellowcard@2x.png />";
