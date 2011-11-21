0a32c3e9fde1741fafbd32ee5b26b868f5760da2
Tom Callon <tomcallon2010@gmail.com>
2011-11-21 12:17:05 +0800

    SelectIndexController :
    1.add some functions which are not implemented .

--- FootballScore/Controller/Index/SelectIndexController.h
+++ FootballScore/Controller/Index/SelectIndexController.h
@@ -33,10 +33,15 @@ enum {
     NSMutableArray *bigandSmallArray;
     
     NSMutableSet *selectedBwin;
+    
+    UIScrollView* buttonScrollView ;
 
     id<SeclectIndexControllerDelegate> delegate;
     
 }
+
+
+@property(nonatomic,retain)  UIScrollView* buttonScrollView ;
 
 @property (nonatomic, retain) IBOutlet UIButton *buttonAsianBwin;
 @property (nonatomic, retain) IBOutlet UIButton *buttonEuropeBwin;
@@ -46,6 +51,14 @@ enum {
 - (IBAction)clickContentTypeButton:(id)sender;
 - (void)buttonClicked:(id)sender;
 - (void)createButtonsByArray:(NSArray*)array;
+
+
+//
+//- (BOOL)isOddsCompanySelected:(NSString*)OddsCompanyId;
+//- (void)selectOddsCompany:(NSString*)OddsCompanyId;
+//- (void)deSelectOddsCompany:(NSString*)OddsCompanyId;
+//
+
 
 + (SelectIndexController*)show:(UIViewController<SeclectIndexControllerDelegate>*)superController;
 + (UIScrollView*)createButtonScrollViewByButtonArray:(NSArray*)buttons 
