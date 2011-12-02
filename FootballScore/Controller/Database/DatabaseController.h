dda4f36bc0ffcc2d919bf32ae12de912d123ae9e
GamyHuang <hguangm2009@gmail.com>
2011-12-02 17:35:02 +0800

    add databaseController.

---
+++ FootballScore/Controller/Database/DatabaseController.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseController.h
+//  FootballScore
+//
+//  Created by  on 11-12-2.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DatabaseController : UIViewController
+
+@end
